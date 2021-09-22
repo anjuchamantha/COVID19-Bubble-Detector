3b070a0f03ccb98a3ff321347f42476bb44883c0
Achinths Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2021-09-22 12:03:02 +0530

    landing page one added

--- bubble_detector/lib/main.dart
+++ bubble_detector/lib/main.dart
@@ -1,6 +1,3 @@
-import 'package:bubble_detector/bindings/splash_bindings.dart';
-import 'package:bubble_detector/util/theme.dart';
-import 'package:bubble_detector/views/pages/splash_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -8,7 +5,11 @@ import 'package:get/get.dart';
 
 import 'bindings/app_binding.dart';
 import 'bindings/home_bindings.dart';
+import 'bindings/splash_bindings.dart';
+import 'util/theme.dart';
+import 'views/pages/Landing/landing_one.dart';
 import 'views/pages/home_page.dart';
+import 'views/pages/splash_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,12 +23,16 @@ void main() async {
     GetMaterialApp(
       initialBinding: AppBinding(),
       theme: PrimaryTheme.buildTheme(),
-      initialRoute: '/splash',
+      initialRoute: '/landing_one',
       getPages: [
         GetPage(
           name: '/splash',
           page: () => SplashPage(),
           binding: SplashBinding(),
+        ),
+        GetPage(
+          name: '/landing_one',
+          page: () => LandingOne(),
         ),
         GetPage(
           name: '/home',
