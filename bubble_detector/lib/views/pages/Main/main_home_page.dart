d24a2857bba9803888a13aad6c3552b748a88ef3
Achinths Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2021-09-25 19:38:44 +0530

    tab bar added
    But need to add tab controller to GetX controller to dynamically change the icons in the tabbar

--- bubble_detector/lib/views/pages/Main/main_home_page.dart
+++ bubble_detector/lib/views/pages/Main/main_home_page.dart
@@ -6,11 +6,55 @@ class MainHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12.0),
-        child: SafeArea(
-          child: EmergencyContactPage(),
+    //TabController _tabController = new TabController(length: 4, vsync: this)
+
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home_outlined)),
+              Tab(icon: Icon(Icons.history)),
+              Tab(icon: Icon(Icons.directions_bike)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+            labelColor: Theme.of(context).primaryColor,
+            unselectedLabelColor: Theme.of(context).scaffoldBackgroundColor,
+            indicatorColor: Colors.transparent,
+            indicatorWeight: 0.1,
+          ),
+          title: Image(
+            image: AssetImage('images/logo.png'),
+            width: 150,
+          ),
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        ),
+        body: Column(
+          children: [
+            Container(
+              child: Divider(),
+              transform: Matrix4.translationValues(0.0, -6.0, 0.0),
+            ),
+            Expanded(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12.0, vertical: 0),
+                child: SafeArea(
+                  child: TabBarView(
+                    children: [
+                      EmergencyContactPage(),
+                      EmergencyContactPage(),
+                      EmergencyContactPage(),
+                      EmergencyContactPage(),
+                    ],
+                    physics: BouncingScrollPhysics(),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
