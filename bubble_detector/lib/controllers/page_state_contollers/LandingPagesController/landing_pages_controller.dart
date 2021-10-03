c0345514e6bdac3882b07b8b9bb956a9bd6c4883
Achinths Isuru <achinthaisuru.17@cse.mrt.ac.lk>
2021-10-03 12:16:50 +0530

    landing page routing done

--- bubble_detector/lib/controllers/page_state_contollers/LandingPagesController/landing_pages_controller.dart
+++ bubble_detector/lib/controllers/page_state_contollers/LandingPagesController/landing_pages_controller.dart
@@ -1,3 +1,5 @@
+import 'package:bubble_detector/views/pages/Landing/landing_otp.dart';
+import 'package:bubble_detector/views/pages/Landing/landing_phone_number.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -6,15 +8,10 @@ import 'landing_page_state.dart';
 class LandingPagesController extends GetxController {
   RxInt stepCounter = 1.obs;
   RxInt landingPageState = 1.obs;
-  //Rx<LandingPageState> landingPageState = LandingPagePhoneNumberState().obs;
-
   void increaseStepCounter() => stepCounter++;
 
-  // This returns the next state of the Landing Page Series
-  /*  void nextLandingPage() {
-    landingPageState = landingPageState.value.nextState().obs;
-    print(landingPageState.value.runtimeType);
-  } */
-
+  /// This returns the next state of the Landing Page Series
+  /// [1] - [LandingPhoneNumber]
+  /// [2] - [LandingOtp]
   void nextLandingPage() => landingPageState++;
 }
