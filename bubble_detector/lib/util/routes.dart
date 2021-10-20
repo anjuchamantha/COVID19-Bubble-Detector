239ec5903101b23f58549a5a2abbfe6414d0df9f
Anju Chamantha <chamantha97anju@gmail.com>
2021-10-20 11:25:08 +0530

    -

--- bubble_detector/lib/util/routes.dart
+++ bubble_detector/lib/util/routes.dart
@@ -3,7 +3,6 @@ class AppRoutes {
   static const String SPLASH = '/splash';
   static const String LANDING_ONE = '/landing_one';
   static const String LANDING_OUTER = '/landing_outer';
-  static const String MAIN_HOME_PAGE = '/main_home_page';
   static const String HOME = '/home';
   static const String SECOND = '/second';
 }
