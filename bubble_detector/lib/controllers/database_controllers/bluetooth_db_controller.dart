4a5dc2c5e98b9b6bafdf7ac555716d012dfb7130
Anju Chamantha <chamantha97anju@gmail.com>
2021-12-17 17:53:50 +0530

    Put contacted users in a new collection

--- bubble_detector/lib/controllers/database_controllers/bluetooth_db_controller.dart
+++ bubble_detector/lib/controllers/database_controllers/bluetooth_db_controller.dart
@@ -23,27 +23,43 @@ class BluetoothDBController extends GetxController {
     });
   }
 
-  Future<void> updateContactedUsers(RxMap<dynamic, dynamic> beaconMsgs) {
-    var contacts = [];
-    beaconMsgs.forEach((k, b) {
-      contacts.add({
+  updateContactedUsers(RxMap<dynamic, dynamic> beaconMsgs) async {
+    DocumentReference userRef =
+        FirebaseFirestore.instance.collection('users').doc(user!.uid);
+
+    // var contacts = [];
+
+    // beaconMsgs.forEach((k, b) {
+    //   contacts.add({
+    //     "user": b.phone,
+    //     "distance": b.accuracy,
+    //     "timestamp": DateTime.now()
+    //   });
+    // });
+
+    // // Call the user's CollectionReference to add a new user
+    // await userRef.update({
+    //   'contacts': FieldValue.arrayUnion(contacts),
+    // }).then((value) {
+    //   print("Contacted Users updated");
+    //   Get.snackbar("Successfull", "Contacted Users updated");
+    // }).catchError((error) {
+    //   print("Failed to update user Contacted Users: $error");
+    //   Get.snackbar("ERROR", "Failed to update Contacted Users");
+    // });
+
+    var batch = firestore.batch();
+
+    beaconMsgs.forEach((key, b) {
+      var docRef = userRef.collection("contacts_collection").doc();
+      batch.set(docRef, {
         "user": b.phone,
         "distance": b.accuracy,
         "timestamp": DateTime.now()
       });
     });
-    DocumentReference userRef =
-        FirebaseFirestore.instance.collection('users').doc(user!.uid);
-    // Call the user's CollectionReference to add a new user
-    return userRef.update({
-      'contacts': FieldValue.arrayUnion(contacts),
-    }).then((value) {
-      print("Contacted Users updated");
-      Get.snackbar("Successfull", "Contacted Users updated");
-    }).catchError((error) {
-      print("Failed to update user Contacted Users: $error");
-      Get.snackbar("ERROR", "Failed to update Contacted Users");
-    });
+
+    await batch.commit();
   }
 
   // getUsersWithBluetoothID(List<String> bluetoothIDs) async {
